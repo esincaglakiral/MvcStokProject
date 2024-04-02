63c9a71862800f13528f65ccbf13b77085d8a83b
Esin Çağla KIRAL <caglakiral@gmail.com>
2024-04-02 07:58:12 +0300

    add project

---
+++ MvcStok/Controllers/KategoriController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+using PagedList;
+using PagedList.Mvc;
+
+namespace MvcStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        MvcDbStokEntities db = new MvcDbStokEntities();
+
+        public ActionResult Index(int sayfa = 1)
+        {
+            
+            var degerler = db.TBLKATEGORILER.ToList().ToPagedList(sayfa, 5); //Yukarıdaki satırdan farkı, verilerin sayfalanmış bir şekilde gelmesidir. Sondaki (1,4) ise 1 sayfada 4 veri olması demektir.
+
+            return View(degerler);
+        }
+
+        [HttpGet]  //[HttpGet] ile sadece sayfayı görüntüleriz.
+        public ActionResult YeniKategori()
+        {
+            return View();
+        }
+
+        [HttpPost] // sayfaya herhangi bir post işlemi yapıldğı zaman yani mesela ben butona bastığım zaman burdaki işlemi gerçekleştir aksi durumda [HttpGet] ile sadece sayfayı görüntüleriz.
+        public ActionResult YeniKategori(TBLKATEGORILER kategori)
+        {
+            if (!ModelState.IsValid) //Kategori adının boş geçilip geçilmediğinin kontrolü
+            {
+                return View("YeniKategori");
+            }
+            db.TBLKATEGORILER.Add(kategori);
+            db.SaveChanges();
+            return View();
+
+        }
+
+        public ActionResult kategoriSil(int id) //Seçilen kategorinin silinmesi
+        {
+            var kategori = db.TBLKATEGORILER.Find(id);
+            db.TBLKATEGORILER.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // her actionresult sayfası post edildiğinde ya da refresh yapıldığında yeni ekleme işlemi gerçekleşiyor. o zaman benim ben butona basana kadar ekleme
+        // işlemi gerçekleştirmemesi gerek. 
+
+
+        public ActionResult kategoriGetir(int id) //Güncellenecek olan kategorinin ID' sini getirme
+        {
+            var kategori = db.TBLKATEGORILER.Find(id);
+            return View("kategoriGetir", kategori);
+        }
+
+
+        public ActionResult kategoriGuncelle(TBLKATEGORILER guncellenecekKategori) //Kategori güncelleme işlemi
+        {
+            var kategori = db.TBLKATEGORILER.Find(guncellenecekKategori.KATEGORIID);
+            kategori.KATEGORIAD = guncellenecekKategori.KATEGORIAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcStok/Controllers/MusteriController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+using PagedList;
+using PagedList.Mvc;
+
+namespace MvcStok.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        MvcDbStokEntities db = new MvcDbStokEntities();
+
+        public ActionResult Index(int sayfa = 1, string searchString = "")
+        {
+            var musteriList = db.TBLMUSTERILER.ToList();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                musteriList = musteriList.Where(m => m.MUSTERIAD.Contains(searchString) || m.MUSTERISOYAD.Contains(searchString)).ToList();
+            }
+
+            var model = musteriList.ToPagedList(sayfa, 5);
+
+            ViewBag.CurrentFilter = searchString; // Filtre değerini saklarız
+            ViewBag.CurrentPage = sayfa; // Geçerli sayfa numarasını saklarız
+
+            return View(model);
+        }
+
+
+
+        [HttpGet]  //[HttpGet] ile sadece sayfayı görüntüleriz.
+        public ActionResult YeniMusteri()
+        {
+            return View();
+        }
+
+        [HttpPost] 
+        public ActionResult YeniMusteri(TBLMUSTERILER musteri)
+        {
+            if (!ModelState.IsValid) //Müşteri adının boş geçilip geçilmediğinin kontrolü
+            {
+                return View("YeniMusteri");
+            }
+            db.TBLMUSTERILER.Add(musteri);
+            db.SaveChanges();
+            return View();
+
+        }
+
+        public ActionResult MusteriSil(int id) //Seçilen kategorinin silinmesi
+        {
+            var musteri = db.TBLMUSTERILER.Find(id);
+            db.TBLMUSTERILER.Remove(musteri);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult musteriGetir(int id) //Güncellenecek olan kategorinin ID' sini getirme
+        {
+            var musteri = db.TBLMUSTERILER.Find(id);
+            return View("musteriGetir", musteri);
+        }
+
+
+        public ActionResult musteriGuncelle(TBLMUSTERILER guncellenecekMusteri) //Kategori güncelleme işlemi
+        {
+            var musteri = db.TBLMUSTERILER.Find(guncellenecekMusteri.MUSTERIID);
+            musteri.MUSTERIAD = guncellenecekMusteri.MUSTERIAD;
+            musteri.MUSTERISOYAD = guncellenecekMusteri.MUSTERISOYAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
